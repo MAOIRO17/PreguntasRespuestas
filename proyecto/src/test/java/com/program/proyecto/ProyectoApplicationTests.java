2000ba0bc9ca260094dd5c696b017562068281b7
MAOIRO17 <maoiro17@gmail.com>
2025-06-05 16:45:46 +0200

    creación de proyecto con Java Spring

---
+++ proyecto/src/test/java/com/program/proyecto/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.program.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
