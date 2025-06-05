511836b75d4ddeb7c51d26d5e4e2e784d5d0d2a3
MAOIRO17 <maoiro17@gmail.com>
2025-06-05 17:14:01 +0200

    creación del servicio(EncuestaService)

---
+++ proyecto/src/main/java/com/project/services/EncuestaService.java
@@ -0,0 +1,34 @@
+package com.project.services;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+import com.project.model.Encuesta;
+import com.project.model.Opcion;
+import com.project.repository.EncuestaRepository;
+import com.project.repository.OpcionRepository;
+
+@Service
+public class EncuestaService {
+    private EncuestaRepository encuestaRepository;
+    private OpcionRepository opcionRepository;
+
+    public List<Encuesta> getAllEncuestas() {
+        return encuestaRepository.findAll();
+    }
+
+    public Optional<Encuesta> getEncuestaById(Long id) {
+        return encuestaRepository.findById(id);
+    }
+
+    public Encuesta guardarEncuesta(Encuesta encuesta) {
+        return encuestaRepository.save(encuesta);
+    }
+
+    public void votar(Long idOpcion) {
+        Opcion opcion = opcionRepository.findById(idOpcion)
+                .orElseThrow(() -> new RuntimeException("Opción no encontrada"));
+        opcion.setVotos(opcion.getVotos() + 1);
+        opcionRepository.save(opcion);
+    }
+}
