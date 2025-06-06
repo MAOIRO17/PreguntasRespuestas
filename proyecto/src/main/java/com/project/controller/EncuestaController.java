df7b5edab8ed36b4f60fa7133fb6f5f3b9460131
MAOIRO17 <maoiro17@gmail.com>
2025-06-06 16:06:14 +0200

    creación del controlador(EncuestaController)

---
+++ proyecto/src/main/java/com/project/controller/EncuestaController.java
@@ -0,0 +1,33 @@
+package com.project.controller;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.project.model.Encuesta;
+import com.project.services.EncuestaService;
+
+@RestController
+@RequestMapping("/encuestas")
+public class EncuestaController {
+
+    private EncuestaService encuestaService;
+
+    @GetMapping
+    public List<Encuesta> listarEncuestas() {
+        return encuestaService.getAllEncuestas();
+    }
+
+    @PostMapping
+    public Encuesta crearEncuesta(@RequestBody Encuesta encuesta) {
+        return encuestaService.guardarEncuesta(encuesta);
+    }
+
+    @PostMapping("/votar/{idOpcion}")
+    public void votar(@PathVariable Long idOpcion) {
+        encuestaService.votar(idOpcion);
+    }
+}
